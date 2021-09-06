44ac4b0290dc3a95263d886c3372e2cf70f5149b
leandro <leandrochagask@gmail.com>
2021-09-06 15:11:16 -0300

    Adiciona exercicio 01 parte 2

--- fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-4-javascript-objetos-e-funcoes/exercicio2_1.js
+++ fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-4-javascript-objetos-e-funcoes/exercicio2_1.js
@@ -2,3 +2,17 @@
 // Crie uma função que receba uma string e retorne true se for um palíndromo , ou false , se não for.
 
 
+function verificaPalindrome(params) { 
+  let message = 'true'
+  for (let i = 0; i < params.length; i += 1) {
+    if (params[i] !== params[params.length - i - 1]) {
+      message = 'false';
+      break;
+    }  
+  }
+  return message;
+}
+
+let resultado = verificaPalindrome('arara');
+
+console.log(resultado);
