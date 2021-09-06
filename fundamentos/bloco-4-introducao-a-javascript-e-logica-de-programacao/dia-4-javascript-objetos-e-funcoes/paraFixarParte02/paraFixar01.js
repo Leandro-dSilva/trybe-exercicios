b40b99bfb60c9272ccc2185d218488a43857b6f1
leandro <leandrochagask@gmail.com>
2021-09-06 11:46:41 -0300

    atividade para fixar 01, parte 2.

---
+++ fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-4-javascript-objetos-e-funcoes/paraFixarParte02/paraFixar01.js
@@ -0,0 +1,13 @@
+// Para fixar 01
+// Usando o objeto abaixo, utilize For/in e imprima no console a mensagem 'Olá xxxxx' para cada nome, substituindo o xxxxx pelo nome em questão
+
+let names = {
+  person1: 'João',
+  person2: 'Maria',
+  person3: 'Jorge' 
+};
+
+for (let name in names) {
+  let message = 'Olá ' + names[name] + '.';
+  console.log(message);
+}
