9e2ab82003d6ece304f474a3a23b99b040835f6e
leandro <leandrochagask@gmail.com>
2021-09-08 16:00:29 -0300

    Complementa exercício 01

--- fundamentos/bloco-5-javascript-dom-eventos-e-web-storage/dia-3-javascript-eventos/script.js
+++ fundamentos/bloco-5-javascript-dom-eventos-e-web-storage/dia-3-javascript-eventos/script.js
@@ -18,17 +18,47 @@ createDaysOfTheWeek();
 // Exercício 1
 const dezDaysList = [29, 30, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31];
 
+//posiciona days
 const ulReceptor = document.querySelector('#days')
 
 for (let i = 1; i < dezDaysList.length; i += 1) {
   let dayMonth = dezDaysList[i];
 
-  let newli = document.createElement('li');
-  newli.innerText = dayMonth;
-  newli.classList = 'day';
+  let newLi = document.createElement('li');
+  newLi.innerText = dayMonth;
+  newLi.classList = 'day';
   
-  ulReceptor.appendChild(newli);        
+  ulReceptor.appendChild(newLi);        
 }
+
+//marca feriados
+let holidays = [24, 25, 31];   //marca numeros inseridos aqui
+let day = document.getElementsByClassName('day');  
+
+for (let i = 0; i < holidays.length; i += 1) {
+  let holiday = holidays[i];
+  for (let i = 0; i < day.length; i += 1) {
+    let test = day[i].innerText;
+    if (holiday == test) {
+      day[i].classList.add('holiday');
+    }
+  }
+}
+
+// marca sexta-feira
+let fridays = [4, 11, 18, 25];   //marca numeros inseridos aqui
+
+for (let i = 0; i < fridays.length; i += 1) {
+  let friday = fridays[i];
+  for (let i = 0; i < day.length; i += 1) {
+    let test = day[i].innerText;
+    if (friday == test) {
+      day[i].classList.add('friday');
+    }
+  }
+}
+
+
 
 // Exercício 2
 
