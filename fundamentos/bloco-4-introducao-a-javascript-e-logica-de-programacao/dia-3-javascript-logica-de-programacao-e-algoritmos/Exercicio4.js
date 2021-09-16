c87cbbb01b0ede34c1fd482aa90fc91c86b55dc7
leandro <leandrochagask@gmail.com>
2021-09-16 10:20:08 -0300

    Adiciona arquivos

--- fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-3-javascript-logica-de-programacao-e-algoritmos/Exercicio4.js
+++ fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-3-javascript-logica-de-programacao-e-algoritmos/Exercicio4.js
@@ -7,17 +7,21 @@
 //  ***
 // *****
 
-let n = 8;
+let n = 5;
 let a = '';
 let l;
 
 for (let i = 0; i <= n; i += 1) { 
   a += '*';
+  
   if (i%2 === 0) {
     let x = (n-i)/2;
     let e = '';
+  
     for (let i = 0; i < x; i += 1) {
+  
       e += ' ';      
+  
     }
     l = e + a + e;
     console.log(l);
