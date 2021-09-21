1d0a766177e7880dacc18e80447b31e657acac57
leandro <leandrochagask@gmail.com>
2021-09-21 13:31:14 -0300

    Adiciona select com estados

--- fundamentos/bloco-6-html-e-css-forms-flexbox-e-responsivo/dia-1-html-css-forms/script.js
+++ fundamentos/bloco-6-html-e-css-forms-flexbox-e-responsivo/dia-1-html-css-forms/script.js
@@ -1,9 +1,48 @@
+let stateSelect = document.querySelector('#state');
 let dateInput = document.querySelector('#date');
 let submitButton = document.querySelector('#submit-button');
 let messageError = document.querySelector('#message-error')
 
+let states = [
+ 'Acre',
+ 'Alagoas',
+ 'Amapá',
+ 'Amazonas',
+ 'Bahia',
+ 'Ceará',
+ 'Distrito Federal',
+ 'Espírito Santo',
+ 'Goías',
+ 'Maranhão',
+ 'Mato Grosso',
+ 'Mato Grosso do Sul',
+ 'Minas Gerais',
+ 'Pará',
+ 'Paraíba',
+ 'Paraná',
+ 'Pernambuco',
+ 'Piauí',
+ 'Rio de Janeiro',
+ 'Rio Grande do Norte',
+ 'Rio Grande do Sul',
+ 'Rondônia',
+ 'Roraíma',
+ 'Santa Catarina',
+ 'São Paulo',
+ 'Sergipe',
+ 'Tocantins'
+];
+
+for (let i = 0; i < states.length; i +=1) {
+  let state = states[i];
+  let option = document.createElement('option');
+  option.innerText = `${state}`;
+
+  stateSelect.appendChild(option);
+}
+
 submitButton.addEventListener('click', (event) => {
-  event.preventDefault();
+  // event.preventDefault();
   messageError.innerText = '';
   let dateValue = dateInput.value;
   let dateSplit = dateValue.split('/')
