be80ad60d65aacc6472dfac89c35bd78e8a983c1
leandro <leandrochagask@gmail.com>
2021-09-21 23:16:30 -0300

    Adiciona date picker

--- fundamentos/bloco-6-html-e-css-forms-flexbox-e-responsivo/dia-2-bibliotecas-javascript-e-frameworks-css/script.js
+++ fundamentos/bloco-6-html-e-css-forms-flexbox-e-responsivo/dia-2-bibliotecas-javascript-e-frameworks-css/script.js
@@ -70,16 +70,5 @@ function confirm() {
 
 submitButton.addEventListener('click', (event) => {
   event.preventDefault();
-  messageError.innerText = '';
-  let dateValue = dateInput.value;
-  let dateSplit = dateValue.split('/')
-
-  let validation = (dateSplit[0] > 0 && dateSplit[0] <= 31) && (dateSplit[1] >0 && dateSplit[1] <= 12) && (dateSplit[2] > 0);
-
-  if (!validation) {
-    messageError.innerText = 'Preencha uma data válida no formato dd/mm/aaaa'
-  } else {
-    confirm();
-  }
+  confirm();
 });
-
