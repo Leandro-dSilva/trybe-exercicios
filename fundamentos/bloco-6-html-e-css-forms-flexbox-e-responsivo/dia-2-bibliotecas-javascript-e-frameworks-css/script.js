55fb3df7029c6b3dd63370eec618fc37ba3ded03
leandro <leandrochagask@gmail.com>
2021-09-23 13:26:33 -0300

    Adiciona validacao

--- fundamentos/bloco-6-html-e-css-forms-flexbox-e-responsivo/dia-2-bibliotecas-javascript-e-frameworks-css/script.js
+++ fundamentos/bloco-6-html-e-css-forms-flexbox-e-responsivo/dia-2-bibliotecas-javascript-e-frameworks-css/script.js
@@ -36,3 +36,52 @@ for (let i = 0; i < states.length; i +=1) {
   option.innerText = `${state}`;
   stateSelect.appendChild(option);
 }
+
+new JustValidate('.js-form', {
+  rules: {
+    name: {
+      required: true
+    },
+    cpf: {
+      required: true
+    },
+    addressInput: {
+      required: true
+    },
+    cityInput: {
+      required: true
+    },
+    state: {
+      required: true
+    },
+    radio: {
+      required: true
+    },
+    textArea: {
+      required: true
+    },
+    office: {
+      required: true
+    },
+    JobDescription: {
+      required: true
+    },
+    datepicker: {
+    required: true
+    }
+  },
+  messages: {
+ 
+    name: 'Campo Obrigatório',
+    email: 'Campo Obrigatório',
+    cpf: 'Campo Obrigatório',
+    addressInput: 'Campo Obrigatório',
+    cityInput: 'Campo Obrigatório',
+    state: 'Campo Obrigatório',
+    radio: 'Campo Obrigatório',
+    textArea: 'Campo Obrigatório',
+    office: 'Campo Obrigatório',
+    JobDescription: 'Campo Obrigatório',
+    datepicker: 'Campo Obrigatório'
+  },
+});
