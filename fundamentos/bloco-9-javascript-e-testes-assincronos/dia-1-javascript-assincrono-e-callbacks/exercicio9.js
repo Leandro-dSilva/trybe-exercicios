9c1d0d5f5af1b3c5eafacd27a2fc143262e41f0e
leandro <leandrochagask@gmail.com>
2021-10-18 12:21:06 -0300

    Remove console.log

---
+++ fundamentos/bloco-9-javascript-e-testes-assincronos/dia-1-javascript-assincrono-e-callbacks/exercicio9.js
@@ -0,0 +1,3 @@
+// Exercício 09 - A fim de evitar que futuros treinadores sejam prejudicados, o Professor Carvalho pediu que você o ajude a escrever testes para o sistema que distribui os pokémons. Crie um novo arquivo .test.js ou .spec.js e copie o código abaixo. Complete os testes para a função getPokemonDetails de acordo com as especificações.
+
+// (Ver exercicio8.test.js na pasta test)
