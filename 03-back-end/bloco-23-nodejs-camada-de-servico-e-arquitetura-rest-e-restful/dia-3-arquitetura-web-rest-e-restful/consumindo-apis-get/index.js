be359e071e13f1bd4ad7a4ca3e2d20e5fa39eef8
leandrochs <leandrochagask@gmail.com>
2022-03-29 13:58:02 -0300

    feat: exemplo de consumo de apis

---
+++ 03-back-end/bloco-23-nodejs-camada-de-servico-e-arquitetura-rest-e-restful/dia-3-arquitetura-web-rest-e-restful/consumindo-apis-get/index.js
@@ -0,0 +1,31 @@
+// index.js
+const fetch = require('node-fetch');
+
+const API_TOKEN = '2d635ea9b637ea0f27d58985cc161d64';
+
+const headers = new fetch.Headers({
+  Authorization: API_TOKEN,
+});
+
+fetch('https://postman-echo.com/get?param1=teste', {
+  headers,
+})
+  .then((response) => {
+    if (!response.ok) {
+      return Promise.reject(response);
+    }
+
+    return response.json();
+  })
+  .then((data) => {
+    console.log(data);
+  })
+  .catch((errorOrResponse) => {
+    if (errorOrResponse.status) {
+      return console.error(
+        `Request failed with status ${errorOrResponse.status}`,
+      );
+    }
+
+    console.error(errorOrResponse);
+  });
