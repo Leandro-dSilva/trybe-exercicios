6c9a1a4de394a1ec7b6d2de610dbcfcb008d272d
leandrochs <leandrochagask@gmail.com>
2022-04-10 12:47:34 -0300

    feat: adiciona include na chamada finOne de addresses

--- 03-back-end/bloco-24-nodejs-orm-autenticacao/dia-2-orm-associations/atividade-extra/transacoes-testes/index.js
+++ 03-back-end/bloco-24-nodejs-orm-autenticacao/dia-2-orm-associations/atividade-extra/transacoes-testes/index.js
@@ -15,7 +15,7 @@ app.use(bodyParser.json());
   utilizando a configuração `test` do seu `config.{js | json}`
 */
 const sequelize = new Sequelize(
-  process.env.NODE_ENV === 'test' ? config.test : config.development
+  process.env.NODE_ENV === 'test' ? config.test : config.development,
 );
 
 app.get('/employees', async (_req, res) => {
@@ -34,15 +34,13 @@ app.get('/employees', async (_req, res) => {
 app.get('/employees/:id', async (req, res) => {
   try {
     const { id } = req.params;
-    const employee = await Employee.findOne({ where: { id } });
+    const employee = await Employee.findOne({
+      where: { id },
+      include: [{ model: Address, as: 'addresses' }],
+    });
 
     if (!employee)
       return res.status(404).json({ message: 'Funcionário não encontrado' });
-
-    if (req.query.includeAddresses === 'true') {
-      const addresses = await Address.findAll({ where: { employeeId: id } });
-      return res.status(200).json({ employee, addresses });
-    }
 
     return res.status(200).json(employee);
   } catch (e) {
@@ -67,7 +65,6 @@ app.get('/employees/:id', async (req, res) => {
 //   }
 // });
 
-
 // Unmanaged transactions
 app.post('/employees', async (req, res) => {
   const t = await sequelize.transaction();
@@ -89,7 +86,7 @@ app.post('/employees', async (req, res) => {
 
     return res.status(201).json({
       id: employee.id, // esse dado será nossa referência para validar a transação
-      message: 'Cadastrado com sucesso'
+      message: 'Cadastrado com sucesso',
     });
   } catch (e) {
     await t.rollback();
@@ -97,7 +94,6 @@ app.post('/employees', async (req, res) => {
     res.status(500).json({ message: 'Algo deu errado' });
   }
 });
-
 
 // Managed transactions
 // app.post('/employees', async (req, res) => {
@@ -128,7 +124,6 @@ app.post('/employees', async (req, res) => {
 //     res.status(500).json({ message: 'Algo deu errado' });
 //   }
 // });
-
 
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => console.log(`Ouvindo na porta ${PORT}`));
