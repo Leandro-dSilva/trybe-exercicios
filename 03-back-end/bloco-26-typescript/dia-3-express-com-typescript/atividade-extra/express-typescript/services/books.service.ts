59f7c103d75a0233269430056ee18e64d39f8f3a
leandrochs <leandrochagask@gmail.com>
2022-05-13 13:18:30 -0300

    feat: adiciona update do CRUD

--- 03-back-end/bloco-26-typescript/dia-3-express-com-typescript/atividade-extra/express-typescript/services/books.service.ts
+++ 03-back-end/bloco-26-typescript/dia-3-express-com-typescript/atividade-extra/express-typescript/services/books.service.ts
@@ -3,6 +3,7 @@
 import connection from '../models/connection';
 import BookModel from '../models/book.model';
 import Book from '../interfaces/book.interface';
+import { NotFoundError } from 'restify-errors';
 
 class BookService {
   public model: BookModel;
@@ -23,6 +24,15 @@ class BookService {
 
   public create(book: Book): Promise<Book> {
     return this.model.create(book);
+  }
+
+  public async update(id: number, book: Book): Promise<void> {
+    const bookFound = await this.model.getById(id);
+    if (!bookFound) {
+      throw new NotFoundError('NotFoundError');
+    }
+
+    return this.model.update(id, book);
   }
 }
 
