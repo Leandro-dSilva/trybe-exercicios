8d049d3838425a837ef0f8d6d9d7376379c9819e
leandrochs <leandrochagask@gmail.com>
2022-05-13 12:58:24 -0300

    feat: camadas para getById

--- 03-back-end/bloco-26-typescript/dia-3-express-com-typescript/atividade-extra/express-typescript/dist/services/books.service.js
+++ 03-back-end/bloco-26-typescript/dia-3-express-com-typescript/atividade-extra/express-typescript/dist/services/books.service.js
@@ -1,5 +1,5 @@
 "use strict";
-// ./models/book.model.ts
+// ./services/books.service.ts
 var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
     function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
     return new (P || (P = Promise))(function (resolve, reject) {
@@ -9,34 +9,27 @@ var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, ge
         step((generator = generator.apply(thisArg, _arguments || [])).next());
     });
 };
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
 Object.defineProperty(exports, "__esModule", { value: true });
-class BookModel {
-    constructor(connection) {
-        this.connection = connection;
+const connection_1 = __importDefault(require("../models/connection"));
+const book_model_1 = __importDefault(require("../models/book.model"));
+class BookService {
+    constructor() {
+        this.model = new book_model_1.default(connection_1.default);
     }
     getAll() {
         return __awaiter(this, void 0, void 0, function* () {
-            const result = yield this.connection.execute('SELECT * FROM books');
-            const [rows] = result;
-            return rows;
+            const books = yield this.model.getAll();
+            return books;
         });
     }
     getById(id) {
         return __awaiter(this, void 0, void 0, function* () {
-            const result = yield this.connection.execute('SELECT * FROM books WHERE id=?', [id]);
-            const [rows] = result;
-            const [book] = rows;
+            const book = yield this.model.getById(id);
             return book;
         });
     }
-    create(book) {
-        return __awaiter(this, void 0, void 0, function* () {
-            const { title, price, author, isbn } = book;
-            const result = yield this.connection.execute('INSERT INTO books (title, price, author, isbn) VALUES (?, ?, ?, ?)', [title, price, author, isbn]);
-            const [dataInserted] = result;
-            const { insertId } = dataInserted;
-            return Object.assign({ id: insertId }, book);
-        });
-    }
 }
-exports.default = BookModel;
+exports.default = BookService;
