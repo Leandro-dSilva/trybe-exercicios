70927a40ee157d6e7dc5f9bf688c7fe0e9f64067
leandrochs <leandrochagask@gmail.com>
2022-05-19 15:10:41 -0300

    feat: adiciona classe Pawn

---
+++ 03-back-end/bloco-27-poo-solid/dia-3-polimorfismo/atividade-extra/resolucao-de-problema/src/Pawn.ts
@@ -0,0 +1,18 @@
+import Piece from './Piece';
+import isInList from './isInList';
+import { BoardHouse, boardRows } from './types';
+
+export default class Pawn extends Piece {
+  type = 'Peão';
+  get availableMoves() {
+    const column = this.position[0];
+    const row = this.position[1];
+    const rowIndex = boardRows.indexOf(row);
+    if (rowIndex === 7) return [];
+
+    const nextHouse: BoardHouse = [column, boardRows[rowIndex + 1]];
+    if (isInList(nextHouse, this.board.occupiedHouses)) return [];
+
+    return [nextHouse];
+  }
+}
