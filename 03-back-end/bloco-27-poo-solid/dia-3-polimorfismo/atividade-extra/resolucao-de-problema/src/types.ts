b777a5ca5063819a1cf310b5ca6e791ca80a7a59
leandrochs <leandrochagask@gmail.com>
2022-05-19 15:11:57 -0300

    feat: coloca types em arquivo

---
+++ 03-back-end/bloco-27-poo-solid/dia-3-polimorfismo/atividade-extra/resolucao-de-problema/src/types.ts
@@ -0,0 +1,6 @@
+export type BoardColumn = 'A' | 'B' | 'C' | 'D' | 'E' | 'F' | 'G' | 'H';
+export type BoardRow = '1' | '2' | '3' | '4' | '5' | '6' | '7' | '8';
+export type BoardHouse = [BoardColumn, BoardRow];
+
+export const boardColumns: BoardColumn[] = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H'];
+export const boardRows: BoardRow[] = ['1', '2', '3', '4', '5', '6', '7', '8'];
