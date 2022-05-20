aadb954ced1a7becc6aec919c812d6e5700dcec4
leandrochs <leandrochagask@gmail.com>
2022-05-20 11:42:34 -0300

    docs: adiciona explicacao do problema

--- 03-back-end/bloco-27-poo-solid/dia-4-solid-introducao-e-principios-s-o-e-d/atividade-extra/src/index.ts
+++ 03-back-end/bloco-27-poo-solid/dia-4-solid-introducao-e-principios-s-o-e-d/atividade-extra/src/index.ts
@@ -1,5 +1,11 @@
 // ./src/index.ts
 
+// Aplicação para gerenciar a situação de estudantes numa escola. A sua primeira
+// tarefa é criar uma função que, ao ser chamada, determina a aprovação ou não de
+// uma pessoa estudante e atualiza seu status no banco de dados como Aprovada ou
+// Reprovada. Além disso, as notas marcadas de 0% a 100% (0.0 a 1.0) devem ser
+// convertidas para os conceitos de A a F.
+
 type Discipline = {
   name: string;
   grade: number;
