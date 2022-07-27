369a5e481ff2ae7a4837d8428dd648dc07f71953
leandrochs <leandrochagask@gmail.com>
2022-07-27 13:07:56 -0300

    feat: adiciona metodos create e readOne

--- 03-back-end/bloco-30-mongodb-com-nodejs-e-poo/dia-1-mongoose-e-arquitetura-msccamada-model/atividade-de-aula/src/models/MongoModel.ts
+++ 03-back-end/bloco-30-mongodb-com-nodejs-e-poo/dia-1-mongoose-e-arquitetura-msccamada-model/atividade-de-aula/src/models/MongoModel.ts
@@ -1,8 +1,21 @@
+import { isValidObjectId, Model } from 'mongoose';
 import IModel from '../interfaces/IModel';
 
 abstract class MongoModel<T> implements IModel<T> {
-  // atributos...
-  // métodos...
+  protected _model:Model<T>;
+  
+  constructor(model:Model<T>) {
+    this._model = model;
+  }
+
+  public async create(obj:T):Promise<T> {
+    return this._model.create({ ...obj });
+  }
+
+  public async readOne(_id:string):Promise<T | null> {
+    if (!isValidObjectId(_id)) throw Error('InvalidMongoId');
+    return this._model.findOne({ _id });
+  }
 }
 
 export default MongoModel;
