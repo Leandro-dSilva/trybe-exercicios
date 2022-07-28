4df2c51d5c36af8e4a69a136500a4d9d8c49a9c7
leandrochs <leandrochagask@gmail.com>
2022-07-28 15:59:12 -0300

    feat: adiciona FrameRouter em app

--- 03-back-end/bloco-30-mongodb-com-nodejs-e-poo/dia-2-mongoose-e-arquitetura-msccamada-service-e-controller/atividade-de-aula/src/app.ts
+++ 03-back-end/bloco-30-mongodb-com-nodejs-e-poo/dia-2-mongoose-e-arquitetura-msccamada-service-e-controller/atividade-de-aula/src/app.ts
@@ -1,10 +1,12 @@
 import express from 'express';
 import 'express-async-errors';
 import errorHandler from './middlewares/error';
-import routes from './routes/Router';
+import frameRouter from './routes/frame';
+// import routes from './routes/Router';
 
 const app = express();
 app.use(express.json());
+app.use(frameRouter);
 app.use(errorHandler);
 
 export default app;
